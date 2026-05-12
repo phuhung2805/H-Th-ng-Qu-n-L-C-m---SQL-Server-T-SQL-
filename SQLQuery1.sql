5d27ad4d1cb6571de94a1399a03145beace60138
phuhung2805 <phuhung28052005@gmail.com>
2026-05-12 22:16:13 +0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿-- Hợp đồng 2 (của Trần Thị B) nợ quá lâu. Quản lý quyết định thanh lý!
+UPDATE HopDong 
+SET TrangThai = N'Đã thanh lý' 
+WHERE MaHD = 2;
+
+-- Kiểm tra xem Macbook và iPhone đã tự động bị đem đi bán chưa (Nhờ Trigger)
+SELECT TenTS, TrangThai FROM TaiSan WHERE MaHD = 2;
